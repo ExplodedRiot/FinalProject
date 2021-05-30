4347af2f056b942ff547fc78959e490fa9fc8f6e
ExplodedRiot <alive_ananda@yahoo.com>
2021-05-30 19:05:33 +0700

    add

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,6 @@
-<h3>
-Footer Page
-</h3>
+<div class="panel panel-default">
+  <div class="panel-body">
+    Panel content
+  </div>
+  <div class="panel-footer">Panel footer</div>
+</div>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,4 +19,11 @@
     @yield('content')
     {{View::make('footer')}}
 </body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }
+
+</style>
 </html>
