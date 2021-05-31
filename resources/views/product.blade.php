fc3f40657b6f1f112f38822319260a8e12771ee4
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-01 02:02:57 +0700

    ed

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -9,7 +9,7 @@
   </ol>
 
   <div class="carousel-item">
-  <img src="img\slide\slide1.jpg" alt="...">
+  <img src="D:\jobsheet\pwl\jobsheet_6-master\FinalProject\img\slide\slide1.jpg" alt="...">
   <div class="carousel-caption d-none d-md-block">
     <h5>Check Out Our Latest Item!</h5>
     <p>Get Your Sneakers With Best Prices!</p>
