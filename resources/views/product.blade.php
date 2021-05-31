878c2848d274377ac752bebbedd9a51524b80667
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-01 01:59:49 +0700

    add

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,31 +1,19 @@
 @extends('master');
 @section("content")
-<div class="container custom-product">
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<div class="custom-product">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="FinalProject\img\slide\slide1" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="FinalProject\img\slide\slide2" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="FinalProject\img\slide\slide3" alt="Third slide">
-    </div>
+
+  <div class="carousel-item">
+  <img src="FinalProject\img\slide\slide1" alt="...">
+  <div class="carousel-caption d-none d-md-block">
+    <h5>Check Out Our Latest Item!</h5>
+    <p>Get Your Sneakers With Best Prices!</p>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
 </div>
 @endsection
