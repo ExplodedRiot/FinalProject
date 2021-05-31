9c4b9ee801e18cb108cdac3892e1129e5b782124
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-01 01:43:37 +0700

    add

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -9,13 +9,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
+      <img class="d-block w-100" src="FinalProject\img\slide\slide1" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
+      <img class="d-block w-100" src="FinalProject\img\slide\slide2" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
+      <img class="d-block w-100" src="FinalProject\img\slide\slide3" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
