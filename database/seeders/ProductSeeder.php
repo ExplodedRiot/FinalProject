544edea9fa2029822813f9c639ed854649d6e59b
ExplodedRiot <alive_ananda@yahoo.com>
2021-05-31 21:48:42 +0700

    add

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,11 +17,27 @@ class ProductSeeder extends Seeder
     {
         //
         DB::table('Products')->insert([
-            'name'=>'Adidas Lite Racer 2.0',
-            "price"=>'399.000',
+            [
+            'name'=>'Converse CTAS Cheerful Faded Spruce',
+            "price"=>'459.000',
             "description"=>"123456",
-            "category"=>"Adidas",
-            "gallery"=>"FinalProject\img\Adidas\Adidas lite racer 2.0 (IDR 399k).jpg"
+            "category"=>"Converse",
+            "gallery"=>"FinalProject\img\Converse\Converse CTAS cheerful faded spruce (IDR 459k).jpg"
+            ],
+            [
+            'name'=>'Nike Air Force 1 LE',
+            "price"=>'819.000',
+            "description"=>"123456",
+            "category"=>"Nike",
+            "gallery"=>"FinalProject\img\Adidas\Nike Air Force 1 LE.jpg"
+            ],
+            [
+            'name'=>'Vans Era Gum Green',
+            "price"=>'499.000',
+            "description"=>"123456",
+            "category"=>"Vans",
+            "gallery"=>"FinalProject\img\Vans\Vans era gum green (IDR 499k).jpg"
+            ]
         ]);
     }
 }
