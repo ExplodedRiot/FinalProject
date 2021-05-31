28a5a70e1ce0e021cfefa06193ca566beaffc10c
ExplodedRiot <alive_ananda@yahoo.com>
2021-05-31 21:21:10 +0700

    add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,6 @@ class UserController extends Controller
     function login(Request $req)
         {
        $user = User::where(['email'=>$req->email])->first();
-       
        if(!$user || !Hash::check($req->password, $user->$password))
        {
            return "Email or Password not Found.";
